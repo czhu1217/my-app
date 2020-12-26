092f18fe8030e688bdc34c34e9c0a0589eea7c4c
czhu1217 <cindyzhu0830@gmail.com>
2020-12-25 23:06:03 -0500

    Updates

---
+++ precache-manifest.f7f69adbbbd5cbdb1893afb76b3f0f38.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28d3d4ba60c7fe73844a9c61f05d802f",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "893003d2d6c626e67df3",
+    "url": "/my-app/static/css/main.81b2e154.chunk.css"
+  },
+  {
+    "revision": "89efdd901e0d1b469f73",
+    "url": "/my-app/static/js/2.4577e2f8.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/my-app/static/js/2.4577e2f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "893003d2d6c626e67df3",
+    "url": "/my-app/static/js/main.b29ef662.chunk.js"
+  },
+  {
+    "revision": "b806ff52d54f6357d588",
+    "url": "/my-app/static/js/runtime-main.d26e7d40.js"
+  },
+  {
+    "revision": "a13bca2f5e9fc89703607ad4bd7f8b4c",
+    "url": "/my-app/static/media/berlin1.a13bca2f.jpeg"
+  },
+  {
+    "revision": "93ec6f51f6fe0939cfb7ecff8785c131",
+    "url": "/my-app/static/media/bvg1.93ec6f51.png"
+  },
+  {
+    "revision": "50715a14d2615e6ea1a958f3c3049be3",
+    "url": "/my-app/static/media/bvg2.50715a14.jpeg"
+  },
+  {
+    "revision": "071565220bbb5e16551c5cfbafea865f",
+    "url": "/my-app/static/media/bvg3.07156522.jpeg"
+  },
+  {
+    "revision": "23a86a63c5a2e8a7ab73a82f78cdd86a",
+    "url": "/my-app/static/media/coda.23a86a63.png"
+  },
+  {
+    "revision": "54835c1bba540ffe87ded7e8a7964db3",
+    "url": "/my-app/static/media/eeart.54835c1b.png"
+  },
+  {
+    "revision": "32c35e149c0a6e8eec0a2fff4de90f2f",
+    "url": "/my-app/static/media/finders.32c35e14.png"
+  },
+  {
+    "revision": "e08e8ff819c627c217c6532d1d38ee5b",
+    "url": "/my-app/static/media/grip2.e08e8ff8.png"
+  },
+  {
+    "revision": "74e5092f1625fa912da4143c39c6ae38",
+    "url": "/my-app/static/media/gripindoor2.74e5092f.png"
+  },
+  {
+    "revision": "e5f666505878b105bb8b0e84fa75b1a0",
+    "url": "/my-app/static/media/keepers.e5f66650.png"
+  },
+  {
+    "revision": "542ec3e717d0ea76a47280f70bf6d330",
+    "url": "/my-app/static/media/melewi.542ec3e7.png"
+  },
+  {
+    "revision": "89224401795c4ba300ef60a2c6a8a83a",
+    "url": "/my-app/static/media/podcast.89224401.png"
+  },
+  {
+    "revision": "52ee068c7ffb0a60bcd401db6b8699eb",
+    "url": "/my-app/static/media/quora1.52ee068c.png"
+  },
+  {
+    "revision": "96ce9c20f33c3eedb2010026edda2d43",
+    "url": "/my-app/static/media/quora2.96ce9c20.png"
+  },
+  {
+    "revision": "08f3b41b29d17803b361fff48a4ef086",
+    "url": "/my-app/static/media/quora3.08f3b41b.png"
+  },
+  {
+    "revision": "8f2f40a4be7dc8d5b5a2e857fe09af4a",
+    "url": "/my-app/static/media/results.8f2f40a4.png"
+  },
+  {
+    "revision": "38ddf213242ee35274b3eb76da65f211",
+    "url": "/my-app/static/media/rv.38ddf213.png"
+  },
+  {
+    "revision": "cba575e59bbdffb781e0bd668d031beb",
+    "url": "/my-app/static/media/ski.cba575e5.png"
+  },
+  {
+    "revision": "c12514fdfa4edd3529b8399d667af204",
+    "url": "/my-app/static/media/word.c12514fd.png"
+  }
+]);
